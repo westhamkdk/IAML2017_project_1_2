cb562ed60cb2eedebcb0efc7d9025103d77d4c95
unknown <vamos.kim@gmail.com>
2017-10-18 01:11:23 +0900

    initial commit

---
+++ features.py
@@ -0,0 +1,124 @@
+
+import os
+import numpy as np
+from scipy import stats
+import pandas as pd
+import librosa
+
+    # example for computing mfcc feature
+def get_mfcc_example(tids):
+    mfcc_list = []
+    try:
+        for tid in tids:
+            tid, feature = compute_mfcc_example(tid)
+            if feature ==0:
+                raise ValueError('tid %s has feature error')
+            else:
+                mfcc_list.append(feature)
+    except Exception as e:
+        print('{}: {}'.format(tid, repr(e)))
+
+    return np.array(mfcc_list)
+
+def compute_mfcc_example(tid):
+    threshold = 1278900
+    try:
+        filepath = get_audio_path('music/music_training', tid)
+        x, sr = librosa.load(filepath, sr=None, mono=True, duration=29.0)  # kaiser_fast
+        x = x.tolist()
+        if(len(x)<threshold):
+            raise ValueError('song length is shorter than threshold')
+        else:
+            x = x[:1278900]
+        x = np.array(x)
+
+        stft = np.abs(librosa.stft(x, n_fft=2048, hop_length=512))
+        mel = librosa.feature.melspectrogram(sr=sr, S=stft ** 2)
+        del stft
+        f = librosa.feature.mfcc(S=librosa.power_to_db(mel), n_mfcc=20)
+        # f.shape would be (20,2498)
+
+    except Exception as e:
+        print('{}: {}'.format(tid, repr(e)))
+        return tid, 0
+
+
+    return tid, f.tolist()
+
+def feature_examples(tid):
+    # example of various librosa features
+    # please check [https://librosa.github.io/librosa/feature.html]
+    threshold = 1278900
+    try:
+        filepath = get_audio_path('music/music_training', tid)
+        x, sr = librosa.load(filepath, sr=None, mono=True, duration=29.0)  # kaiser_fast
+        x = x.tolist()
+        if(len(x)<threshold):
+            raise ValueError('song length is shorter than threshold')
+        else:
+            x = x[:1278900]
+        x = np.array(x)
+
+        # zero_crossing_rate
+        # returns (1,t)
+        f = librosa.feature.zero_crossing_rate(x, frame_length=2048, hop_length=512)
+
+
+        cqt = np.abs(librosa.cqt(x, sr=sr, hop_length=512, bins_per_octave=12,
+                                 n_bins=7 * 12, tuning=None))
+        assert cqt.shape[0] == 7 * 12
+        assert np.ceil(len(x) / 512) <= cqt.shape[1] <= np.ceil(len(x) / 512) + 1
+
+        # chroma_cqt
+        # returns (n_chroma, t)
+        f = librosa.feature.chroma_cqt(C=cqt, n_chroma=12, n_octaves=7)
+
+        # chroma_cqt
+        # returns (n_chroma, t)
+        f = librosa.feature.chroma_cens(C=cqt, n_chroma=12, n_octaves=7)
+
+        del cqt
+        stft = np.abs(librosa.stft(x, n_fft=2048, hop_length=512))
+        assert stft.shape[0] == 1 + 2048 // 2
+        assert np.ceil(len(x) / 512) <= stft.shape[1] <= np.ceil(len(x) / 512) + 1
+        del x
+
+        # chroma_stft
+        # returns (n_chroma, t)
+        f = librosa.feature.chroma_stft(S=stft ** 2, n_chroma=12)
+
+        # rmse
+        # returns (1,t)
+        f = librosa.feature.rmse(S=stft)
+
+        # spectral_centroid
+        # returns (1,t)
+        f = librosa.feature.spectral_centroid(S=stft)
+
+        # spectral_bandwidth
+        # returns (1,t)
+        f = librosa.feature.spectral_bandwidth(S=stft)
+
+        # spectral_contrast
+        # returns (n_bands+1, t)
+        f = librosa.feature.spectral_contrast(S=stft, n_bands=6)
+
+        # spectral_rolloff
+        # returns (1,t)
+        f = librosa.feature.spectral_rolloff(S=stft)
+
+        # mfcc
+        # returns (n_mfcc, t)
+        mel = librosa.feature.melspectrogram(sr=sr, S=stft ** 2)
+        del stft
+        f = librosa.feature.mfcc(S=librosa.power_to_db(mel), n_mfcc=20)
+
+    except Exception as e:
+        print('{}: {}'.format(tid, repr(e)))
+        return tid, 0
+
+
+def get_audio_path(audio_dir, track_id):
+    tid_str = '{:06d}'.format(track_id)
+    return os.path.join(audio_dir, tid_str[:3], tid_str + '.mp3')
+
---
+++ main.py
@@ -0,0 +1,107 @@
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+from dataloader import DataLoader
+from tensorflow.python.platform import gfile
+from time import strftime, localtime, time
+
+
+### properties
+# General
+# TODO : declare additional properties
+# not fixed (change or add property as you like)
+batch_size = 32
+epoch_num = 100
+
+
+# fixed
+metadata_path = 'dataset/track_metadata.csv'
+# True if you want to train, False if you already trained your model
+### TODO : IMPORTANT !!! Please change it to False when you submit your code
+is_train_mode = False
+### TODO : IMPORTANT !!! Please specify the path where your best model is saved
+### example : checkpoint/run-0925-0348
+checkpoint_path = 'checkpoint'
+# 'track_genre_top' for project 1, 'listens' for project 2
+label_column_name = 'track_genre_top'
+
+
+# Placeholder and variables
+# TODO : declare placeholder and variables
+
+# Build model
+# TODO : build your model here
+
+# Loss and optimizer
+# TODO : declare loss and optimizer operation
+# Train and evaluate
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    saver = tf.train.Saver()
+
+
+
+    if is_train_mode:
+        train_dataloader = DataLoader(file_path='dataset/track_metadata.csv', batch_size=batch_size,
+                                      label_column_name=label_column_name, is_training=True)
+
+        for epoch in range(epoch_num):
+            total_batch = train_dataloader.num_batch
+
+            for i in range(total_batch):
+                batch_x, batch_y = train_dataloader.next_batch()
+                # TODO:  do some train step code here
+
+        print('Training finished !')
+        output_dir = checkpoint_path + '/run-%02d%02d-%02d%02d' % tuple(localtime(time()))[1:5]
+        if not gfile.Exists(output_dir):
+            gfile.MakeDirs(output_dir)
+        saver.save(sess, output_dir)
+        print('Model saved in file : %s'%output_dir)
+    else:
+        # skip training and restore graph for validation test
+        saver.restore(sess, checkpoint_path)
+
+
+    # Validation
+    validation_dataloader = DataLoader(file_path='dataset/track_metadata.csv', batch_size = batch_size, label_column_name = label_column_name, is_training= False)
+
+    average_val_cost = 0
+    for i in range(validation_dataloader.num_batch):
+        batch_x, batch_y = validation_dataloader.next_batch()
+        # TODO : do some loss calculation here
+        # average_cost += loss/validation_dataloader.num_batch
+
+    print('Validation loss : %f'%average_val_cost)
+
+    # accuracy test example
+    # TODO :
+    # pred = tf.nn.softmax(<your network output logit object>)
+    # correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+    # accuracy_op = tf.reduce_mean(tf.cast(correct_prediction, "float"))
+    # avg_accuracy = 0
+    #for i in range(validation_dataloader.num_batch):
+        #batch_x, batch_y = validation_dataloader.next_batch()
+        #acc = accuracy_op.eval({x:batch_x, y: batch_y})
+        # avg_accuracy += acc / validation_dataloader.num_batch
+    # print("Average accuracy on validation set ", avg_accuracy)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
